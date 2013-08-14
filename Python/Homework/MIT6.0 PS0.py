4e91313b2912c51844adf2bd1924b87a55058742
markobrien <mark.obrien@ymail.com>
2013-08-13 21:19:26 -0500

    PS0 Complete in Homework folder

---
+++ Python/Homework/MIT6.0 PS0.py
@@ -0,0 +1,9 @@
+#PS0
+#Name: Mark O'Brien
+#No Collaborators
+#Time Spent: 20 min
+##This is PS0 from MIT 6.00.  Ask BD, Ask last name, then print out last name and bdate in that order.
+bDay=input("What is your birthdate?")
+lName=input("What is your Last Name?")
+print (lName + " " + bDay)
+ 
