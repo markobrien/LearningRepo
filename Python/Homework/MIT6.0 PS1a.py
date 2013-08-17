5923f84339b81e0b25a4be199ad311d9f2ab436d
markobrien <mark.obrien@ymail.com>
2013-08-16 19:25:57 -0500

    Still Working ps1a

--- Python/Homework/MIT6.0 PS1a.py
+++ Python/Homework/MIT6.0 PS1a.py
@@ -7,12 +7,18 @@
 OriBal=float(input("What is the outstanding balance in 00.00 notation?  "))
 AnnInt=float(input("What is the Annual Interest Rate in decimal notation?  "))
 MinRat=float(input("What is the min monthly payment rate in decimal notation?  "))
-
+OutBal=OriBal
 IntPai=(AnnInt/OutBal)*12
 MinMPa=OutBal*MinRat
 PriPai=MinMPa-IntPai
-OutBal=OriBal
-
-for i in range(1,13)
-    inc=i+1
+for month in range(13):
+    
+    
+    
+        
+    print "Month" + month
+    print "Minimm monthly payment: " + MinMPa
+    print "Principle paid: " + PriPai
+    print "Remaining balance: " + OutBal
+    month=month+1
     
