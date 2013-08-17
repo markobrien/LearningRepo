ed64e66654b3b59599a79afdd1fefcc32b291de3
markobrien <mark.obrien@ymail.com>
2013-08-16 22:22:22 -0500

    Cannot get total annual paid to work...

--- Python/Homework/MIT6.0 PS1a.py
+++ Python/Homework/MIT6.0 PS1a.py
@@ -1,24 +1,22 @@
-
 #Problem Set 1a
 #Name: Mark O'Brien
 #Collaborators:  probably Google
-#Time Spent:  30min
+#Time Spent:  4hr
 
-OriBal=float(input("What is the outstanding balance in 00.00 notation?  "))
-AnnInt=float(input("What is the Annual Interest Rate in decimal notation?  "))
-MinRat=float(input("What is the min monthly payment rate in decimal notation?  "))
-OutBal=OriBal
-IntPai=(AnnInt/OutBal)*12
-MinMPa=OutBal*MinRat
-PriPai=MinMPa-IntPai
-for month in range(13):
-    
-    
-    
-        
-    print "Month" + month
-    print "Minimm monthly payment: " + MinMPa
-    print "Principle paid: " + PriPai
-    print "Remaining balance: " + OutBal
+Balanc=float(input("What is the outstanding balance?  "))
+AnnInt=float(input("What is the Annual Interest Rate as decimal?  "))
+MinRat=float(input("What is the min monthly payment rate as decimal?  "))
+TotPai=float(0.0)                                         #Total of interest and principle, init outside of loop
+for month in range(12):
+    IntPai=round((AnnInt/12)*Balanc,2)                  #Calc Interest
+    MinMPa=round(Balanc*MinRat,2)                       #Minimum payment due      
+    PriPai=round(MinMPa-IntPai,2)                       #Principle Paid
+    Balanc=round(Balanc-PriPai,2)                       #Balance after principle paid
     month=month+1
-    
+    print ("Month: ", round(month,2))
+    print ("Minimm monthly payment: ",round(MinMPa,2))
+    print ("Principle paid: ",round(PriPai,2))
+    print ("Remaining balance: ",round(Balanc,2))
+    TotPai=TotPai+round(PriPai+IntPai,2)
+print("Total Paid "),round(TotPai,2)                    #This is not returning a value!!
+print("Remaining Balance",round(Balanc,2))
