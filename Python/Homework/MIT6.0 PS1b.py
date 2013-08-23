cc700888d9b2a03425968003bad23c8419d2ae63
markobrien <mark.obrien@ymail.com>
2013-08-22 22:14:19 -0500

    MIT 6.00 PS1b.py Math is not coming up with the same answers as the test cases, but it looks good and I think it could do a lot with how the round function is used in the test case...I only used round() in the output and left the math raw.
    Signed-off-by: markobrien <mark.obrien@ymail.com>

--- Python/Homework/MIT6.0 PS1b.py
+++ Python/Homework/MIT6.0 PS1b.py
@@ -1,23 +1,29 @@
 #Problem Set:    1b
 #Name:           Mark O'Brien
-#Collaborators:  Google
-#Time Spent:     ?
+#Collaborators:  https://github.com/morganwilde/MIT-opencourceware---6.00/blob/master/2-core-elements-of-a-program/problem-set-1.py
+#Time Spent:     5 hrs
 #Problem B is set to determine the amount needed to pay off the bill w/in a year
 #on a monthly basis using a $10 increment.
 
-Balanc=float(input("What is the outstanding balance?  "))
-AnnInt=float(input("What is the Annual Interest Rate as decimal?  "))
-Payment=0
-Chg_Bal=0
-month=0
-while Chg_Bal>=0 and month<=12:
-    for month in range(12):
-        Balanc=(AnnInt/12*Balanc)+Balanc
-        print(round(Balanc,2))
-        Chg_Bal=Balanc-Payment
-        print(round(Chg_Bal,2))
-        Payment+=10
-        month+=1
-
-
-http://www.snip2code.com/Snippet/2702/MIT-6-00SC--Problem-Set-1b
+balanc = float(input("What is the outstanding balance?  "))
+annual_int = float(input("What is the Annual Interest Rate as decimal?  "))
+chg_bal = balanc
+monthly_int = annual_int / 12
+monthly_bal=0
+payment = 0
+while chg_bal > 0:
+    for month in range(13):
+        if chg_bal > 0:
+            chg_bal = ((1 + monthly_int) * chg_bal) - payment
+            if chg_bal <= 0:
+                break
+    if chg_bal > 0:
+        payment += 10
+        chg_bal = balanc
+    else:
+        print ("RESULT")
+        print ("Monthly payment needed", payment)
+        print ("Number of months needed", month)
+        print (round(chg_bal,2))
+    
+    
